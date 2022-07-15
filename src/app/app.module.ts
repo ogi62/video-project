415d142730640d3c196dbb0f6bbb02b44204154f
Ognjen Dangubic <74620644+ogi62@users.noreply.github.com>
2022-07-15 13:58:35 +0200

    Home component (#1)
    * made home.component and router for comp
    
    * made home.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,11 +14,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
