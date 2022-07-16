c5153562582e72091e17c0bcde1f528ceed2108f
Ognjen Dangubic <74620644+ogi62@users.noreply.github.com>
2022-07-16 21:03:40 +0200

    Make models and show games on screen  (#3)
    * made models and method searchGames for getting the games on home component
    
    * show games on screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HttpErrorResponse, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 import { GaugeModule } from 'angular-gauge';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -15,6 +15,8 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { HomeComponent } from './components/home/home.component';
+import { HttpHeadersInterceptor } from './interceptors/http-headers.interceptor';
+import { HttpErrorsInterceptor } from './interceptors/http-errors.interceptor';
 
 @NgModule({
   declarations: [
@@ -34,7 +36,18 @@ import { HomeComponent } from './components/home/home.component';
     MatTabsModule,
     MatIconModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpHeadersInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpErrorsInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,22 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+    constructor() {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        req = req.clone({
+            setHeaders: {
+                'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+                'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com',
+            },
+            setParams: {
+                key: '64d26c6521764a1ca7164be0ca3b141d'
+            }
+        });
+
+        return next.handle(req);
+    }
+}
