7d29aa7731844612064f7a91398e3073462393da
Ogi62 <danguba962@gmail.com>
2022-07-18 12:13:51 +0200

    made changes to html to dispaly all things for detail game

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Game } from 'src/app/models';
@@ -9,7 +9,7 @@ import { HttpService } from 'src/app/services/http.service';
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit,OnDestroy {
   
   gameRating = 0;
   gameId!: string;
@@ -48,6 +48,16 @@ export class DetailsComponent implements OnInit {
       return '#f7aa38';
     } else {
       return '#ef4655';
+    }
+  }
+
+  ngOnDestroy(): void {
+    if(this.gameSub) {
+      this.gameSub.unsubscribe();
+    }
+
+    if(this.routeSub) {
+      this.routeSub.unsubscribe();
     }
   }
 
